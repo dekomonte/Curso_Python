e4b6dd8b681e7f9f3febc246116eb7726e2cc746
Andressa <andressa.sena@mutua.com.br>
2024-07-25 15:25:16 -0300

    Lógica Básica de Programacão

---
+++ Logica_Programcao_Basica/Parte1.py
@@ -0,0 +1,8 @@
+"""
+Docstrings
+Nao eh um comentario;
+O interpretador le;
+"""
+
+# Isso eh um comentario
+
---
+++ Logica_Programcao_Basica/Parte2_Introducao.py
@@ -0,0 +1,10 @@
+#==========A Funcao Print==========
+print(12, 34, sep="-", end='##\n')
+print(876, 32, sep="Pedro")
+print(1, 7, sep="-")
+#CRLF -> \r\n
+#LF -> \n
+
+#==========Tipo String==========
+#Tipagem = Dinâmica/Forte
+#String -> Textos(dentro de aspas)
