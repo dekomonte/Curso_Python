9c5db99d1d6334602faeae4b5710a90bb7db7d27
Andressa <andressa.sena@mutua.com.br>
2024-07-25 15:55:36 -0300

    Lógica Básica de Programacão

--- Logica_Programcao_Basica/Parte2_Introducao.py
+++ Logica_Programcao_Basica/Parte2_Introducao.py
@@ -9,10 +9,30 @@ print(1, 7, sep="-")
 #Tipagem -> Dinâmica/Forte
 #Tipagem Dinâmica -> O tipo pode ser atribuído dinamicamente pelo Python
 #String -> Textos(dentro de aspas)
-print('Explícito', 'é', 'melhor " do que implícito')
+print("Isso aqui eh uma String!")
 
 #Inverter as aspas de fora se for para as de dentro aparecerem
 print('Luke Skywalker')
 print(1, 'Luke "Skywalker"') #Aspas simples
 print(2, "Luke 'Skywalker'") #Aspas duplas
+
+#==========Tipos Int e Float==========
+print(3)
+print(-7)
+print(0)
+
+print(23.8)
+print(5.9)
+
+#Funcao(Classe) type
+print(type(1164))
+print(type(241.908))
+print(type(-131876))
+print(type("Luke Skywalker"))
+print(type(45),type(-90),type(67.9),type("Olá"))
+
+#==========Tipos Booleano==========
+#Pensar como SIM ou NAO
+print(23 == 23) #True
+print(45 == 98) #False
 
