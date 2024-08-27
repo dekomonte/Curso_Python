98e3d0459bd3622bdf74dd28d5d5af34be5948ec
Andressa <andressa.sena@mutua.com.br>
2024-08-27 14:51:59 -0300

    Operadores

--- Logica_Programacao_Basica/Parte7_EstruturasCondicionais.py
+++ Logica_Programacao_Basica/Parte7_EstruturasCondicionais.py
@@ -9,3 +9,25 @@ else:
     print("Triste")
 
 print("FORA DO BLOCO")
+
+
+#==========Operadores de Comparacao==========
+"""
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+
+#==========Operadores Logicos==========
+"""
+and (e) 
+or (ou) 
+not (não)
+and -> Todas as condições precisam ser verdadeiras
+Se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor
+Também existe o tipo None que é usado para representar um não valor
+"""
