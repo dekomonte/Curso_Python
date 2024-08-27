a87b560e7f6e355d1cec60cd813e0c670c8f7e9d
Andressa <andressa.sena@mutua.com.br>
2024-08-27 14:38:34 -0300

    Estruturas Condicionais

---
+++ Logica_Programacao_Basica/Parte7_EstruturasCondicionais.py
@@ -0,0 +1,11 @@
+#==========Estrutura Condicional==========
+entrada = input("A luz deveria ser apagada?")
+
+if entrada == 'sim':
+    print("Ufa!")
+elif entrada == 'nao':
+    print("SAI DAQUI!!")
+else:
+    print("Triste")
+
+print("FORA DO BLOCO")
---
+++ Logica_Programacao_Basica/input.py
@@ -0,0 +1,14 @@
+nome = input('Qual o seu nome?')
+idade = int(input('Qual a sua idade?'))
+
+print(idade)
+print(type(idade))
+print(nome)
+
+num1 = input('Me dê um número:')
+num2 = input('Me dê outro número:')
+
+int_num1 = int(num1)
+int_num2 = int(num2)
+
+print(int_num1+int_num2)
