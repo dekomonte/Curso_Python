dbcc73ffec01ea7c80dc14c082b198dd89ef03c2
Andressa <andressa.sena@mutua.com.br>
2024-08-29 15:10:27 -0300

    Update

--- Exercicios/Exercicio1.py
+++ Exercicios/Exercicio1.py
@@ -2,15 +2,25 @@
 #informe se este número é par ou ímpar. Caso o usuário não digite um número
 #inteiro, informe que não é um número inteiro.
 
-#num = input("Digite um número inteiro: ")
-
-
-
+num = input("Digite um número inteiro: ")
+p1 = int(num)
+if p1 % 2 == 0:
+    print("Número par")
+else:
+    print("Número ímpar")
 
 #Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
-#descrito, exiba a saudação apropriada. Ex.
+#descrito, exiba a saudação apropriada.
 #Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 
+hora = int(input("Digite uma hora (só números inteiros serão aceitos): "))
+
+if hora >= 0 and hora < 12:
+    print("Bom dia")
+elif hora > 11 and hora < 18:
+    print("Boa tarde")
+else:
+    print("Boa noite")
 
 #Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou
 #menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
