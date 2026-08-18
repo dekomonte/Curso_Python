7131628834ab60fcded43aad28aa6b17dda8b362
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-08-18 10:44:59 -0300

    Update horarios_medicamentos.py - Adição da data de hoje

--- Aplicacoes_Exemplos_Exercicios/horarios_medicamentos.py
+++ Aplicacoes_Exemplos_Exercicios/horarios_medicamentos.py
@@ -1,14 +1,22 @@
 # Horários para tomar remédio - script mais tosco e menos robusto possível
+from datetime import date
 
 # ["nome do remedio", primeiro horario, intervalo]
 remedios = [
-    ["Ibuprofeno", 10, 8],
-    ["Dipirona", 9, 6]
-    # ["Ondansetrona", 8, 8]
+    ["Ibuprofeno", 8, 8],
+    ["Dipirona", 8, 6],
+    ["Ondansetrona", 8, 8],
+    ["Buscopan", 8, 12]
 ]
 
+#Data no padrão BR
+data = date.today()
+data = data.strftime("%d/%m/%Y")
+
+# print(f"===== Medicamentos =====")
+print(f"===== Medicamentos - {data} =====")
 for remedio in remedios:
-    print(remedio[0])
+    print("->",remedio[0])
     hora = remedio[1]
     intervalo = remedio[2]
     while hora < 24:
@@ -16,4 +24,4 @@ for remedio in remedios:
         hora = hora+intervalo
     if hora == 24:
         print(f"00:00")
-    print("\n")
+    #print("\n")
